ed13139ff9f009c52da7a3fef7ce35d33e98777f
BeaverWithBerets <pavel.p1112@gmail.com>
2025-01-22 14:42:35 +0100

    Loading

--- app/src/main/java/com/paulpope/kpkaudiolibrary/data/repository/BookDownloader.java
+++ app/src/main/java/com/paulpope/kpkaudiolibrary/data/repository/BookDownloader.java
@@ -3,10 +3,12 @@ package com.paulpope.kpkaudiolibrary.data.repository;
 import static com.paulpope.kpkaudiolibrary.data.repository.BookLibrary.TEXTBOOKS_FOLDER;
 import static com.paulpope.kpkaudiolibrary.data.repository.BookLibrary.WORKBOOKS_FOLDER;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.Log;
 
 import com.google.firebase.storage.StorageReference;
+import com.paulpope.kpkaudiolibrary.R;
 import com.paulpope.kpkaudiolibrary.data.model.books.BookRef;
 import com.paulpope.kpkaudiolibrary.data.model.books.BookTypes;
 
@@ -14,14 +16,19 @@ import java.io.File;
 
 public class BookDownloader {
     private static final String TAG = "FirebaseBookDownloader";
+    private ProgressDialog progressDialog;
 
-    public void downloadBooks(Context context, BookRef bookRef,DownloadUiUpdateCallback uiUpdatedCallback , DownloadCompletedCallback callback) {
-        // Определяем базовую папку (textbooks или workbooks)
+    public void downloadBooks(Context context, BookRef bookRef, DownloadUiUpdateCallback uiUpdatedCallback, DownloadCompletedCallback callback) {
+        // Показываем ProgressDialog
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setCancelable(false); // Запрещаем закрытие
+        progressDialog.setMessage(context.getString(R.string.downloading_book));
+        progressDialog.show();
+
         String baseFolder = bookRef.getBookType() == BookTypes.Textbook ? TEXTBOOKS_FOLDER : WORKBOOKS_FOLDER;
 
         int[] tasksCompleted = {0};
 
-        // Получаем путь к папке для LanguageLevel
         File baseDir = context.getExternalFilesDir(null);
         if (baseDir == null) {
             Log.e(TAG, "External storage not available");
@@ -30,7 +37,6 @@ public class BookDownloader {
 
         File targetFolder = new File(new File(baseDir, baseFolder), bookRef.getLanguageLevel().name());
 
-        // Убедимся, что папка существует
         if (!targetFolder.exists() && !targetFolder.mkdirs()) {
             Log.e(TAG, "Failed to create target folder: " + targetFolder.getAbsolutePath());
             return;
@@ -46,13 +52,12 @@ public class BookDownloader {
                 .addOnFailureListener(e -> Log.e(TAG, "Failed to list files in folder: " + folderRef.getPath(), e));
     }
 
-    private void downloadFile(StorageReference fileRef, File targetFolder, int[] tasksCompleted, int tasksCount,DownloadUiUpdateCallback uiUpdatedCallback, DownloadCompletedCallback callback) {
-
+    private void downloadFile(StorageReference fileRef, File targetFolder, int[] tasksCompleted, int tasksCount, DownloadUiUpdateCallback uiUpdatedCallback, DownloadCompletedCallback callback) {
         Runnable onComplete = () -> {
-
             uiUpdatedCallback.onUpdate((int) ((tasksCompleted[0] * 100.0) / tasksCount));
             if (tasksCompleted[0] == tasksCount) {
                 callback.onBookLoaded();
+                progressDialog.dismiss(); // Закрываем диалог по завершению скачивания
                 Log.d(TAG, "Firebase downloaded book successfully");
             }
         };
@@ -61,13 +66,13 @@ public class BookDownloader {
             File targetFile = new File(targetFolder, fileRef.getName());
             fileRef.getFile(targetFile)
                     .addOnSuccessListener(taskSnapshot -> {
-                                Log.i(TAG, "File downloaded: " + targetFile.getAbsolutePath());
-                                synchronized (tasksCompleted) {
-                                    tasksCompleted[0]++;
-                                }
-                                onComplete.run();
-                            }
-                    ).addOnFailureListener(e -> Log.e(TAG, "Failed to download file: " + fileRef.getPath(), e));
+                        Log.i(TAG, "File downloaded: " + targetFile.getAbsolutePath());
+                        synchronized (tasksCompleted) {
+                            tasksCompleted[0]++;
+                        }
+                        onComplete.run();
+                    })
+                    .addOnFailureListener(e -> Log.e(TAG, "Failed to download file: " + fileRef.getPath(), e));
         } catch (Exception e) {
             Log.e(TAG, "Error initializing file download for: " + fileRef.getPath(), e);
         }
